c2c02748e1d6e55d190d57b126809ced22fd53bd
ProgramSalamander <464243149@qq.com>
2019-12-31 18:45:02 +0800

    Initial Repository

---
+++ src/main/java/cn/edu/nju/teamwiki/TeamWikiApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.nju.teamwiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamWikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamWikiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/edu/nju/teamwiki/TeamWikiApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.nju.teamwiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamWikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
