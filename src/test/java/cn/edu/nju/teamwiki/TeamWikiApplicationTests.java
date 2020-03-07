9a1b578ac5d9ee58198c57e2ce2e452442dc85e4
ProgramSalamander <464243149@qq.com>
2020-03-07 17:58:35 +0800

    fix user api; fix test error

--- src/test/java/cn/edu/nju/teamwiki/TeamWikiApplicationTests.java
+++ src/test/java/cn/edu/nju/teamwiki/TeamWikiApplicationTests.java
@@ -6,6 +6,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TeamWikiApplicationTests {
 
+	// FIXME 测试时jenkins环境下的office和docker环境下的office路径不同。
+	// 		 此配置仅在172.19.241.57上有效。
+	static {
+		System.setProperty("jodconverter.local.officeHome", "/opt/libreoffice6.4");
+	}
+
 	@Test
 	void contextLoads() {
 	}
