4531d3b6dbc6678c6a829ef1baaf2d600ac0fcc6
ProgramSalamander <464243149@qq.com>
2020-04-08 13:20:53 +0800

    update pipeline

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,7 @@
 docker build -t teamwiki:latest .
 docker stop teamwiki
 docker rm teamwiki
-docker run --name teamwiki --detach \
+docker run --name teamwiki --network teamwiki --detach \
  -p 8081:8081 \
  -v teamwiki-data:/var/data/teamwiki \
  -v teamwiki-log:/var/log/teamwiki \
